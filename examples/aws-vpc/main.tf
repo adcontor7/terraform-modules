85fb9d5211383c76a073403eab83b30807f60208
adcontor <adcontor7@gmail.com>
2019-12-14 19:21:21 +0100

    - Modules doc generator

--- examples/aws-vpc/main.tf
+++ examples/aws-vpc/main.tf
@@ -5,6 +5,13 @@ provider "aws" {
 module "network" {
   source = "git@github.com:adcontor7/terraform-modules.git//modules/aws-network?ref=v0.0.1"
   prefix  = "terraform-example"
+
+
+  network_cidr        = "10.0.0.0/16"
+  public_subnet_cidr  = "10.0.1.0/24"
+  private_subnet_cidr = "10.0.2.0/24"
+  enable_nat_gateway  = false
+
   providers = {
     aws = aws
   }
